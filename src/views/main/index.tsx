e489ed290dcf3d6179f3cd3a0204c059a97611ed
jiwoo004 <thswldn0417@naver.com>
2025-01-15 19:20:37 +0900

    메인 헤더

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,7 +1,29 @@
-export default function Main() {
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import './style.css';
+
+const Main: React.FC = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [menuStyle, setMenuStyle] = useState(false);
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    setIsScrolled(scrollY > window.innerHeight / 2);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div>
-      <h1>Main</h1>
+    <div className="home-container">
+      <Header isScrolled={isScrolled}/>
+      <main className="main">
+       
+      </main>
     </div>
-  )
+  );
 }
+
+export default Main;
