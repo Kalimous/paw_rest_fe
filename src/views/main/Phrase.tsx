520537f4983ea1f9e135bc5b0d2b588e0bf48dca
Kim Young Woo <85552569+hppYW@users.noreply.github.com>
2025-01-27 19:06:40 +0900

    1906

--- src/views/main/Phrase.tsx
+++ src/views/main/Phrase.tsx
@@ -5,9 +5,11 @@ const Phrase = () => {
     return(
         <div className='phrase-container'>
             <div className='phrase-content'>
-                <span>유기동물을 입양하셨다면<br/>
-                    <h2>예방접종</h2>과 <h2>건강검진</h2>은 필수!!!
-                </span>      
+                <span>유기동물을 입양하셨다면<br/></span>
+                <h2 className='a'>예방접종</h2>
+                <span>과 </span> 
+                <h2 className='b'>건강검진</h2>
+                <span>은 필수!!!</span>      
             </div>
             <img src={phraseimg} alt='phraseimg' className='phrase-img'/>
         </div>
