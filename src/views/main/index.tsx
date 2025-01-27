2f476f48cb946a7d0dfb4bf4ff6f609efdb5900c
jiwoo004 <thswldn0417@naver.com>
2025-01-27 19:38:45 +0900

    Revert "Merge branch 'develop' into duddn"
    This reverts commit 7202ec83992d8b3bb7a81c26dd6774f4bc45d8ff, reversing
    changes made to 64a35bb9430ca063fdfe83734482bd9cd4d84f8d.

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -7,6 +7,7 @@ import './style.css';
 
 const Main: React.FC = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [menuStyle, setMenuStyle] = useState(false);
 
   const handleScroll = () => {
     const scrollY = window.scrollY;
